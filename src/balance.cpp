52e22341635f40da9a383c3985d5a43c2520d001
Nguyen Tran Thu Trang <trangnguyen@Trangs-MacBook-Air-4.local>
2025-10-16 14:32:39 -0400

    submit

--- src/balance.cpp
+++ src/balance.cpp
@@ -4,10 +4,15 @@
 static bool is_balanced_rec(const std::string& s, int idx, int open) {
     // TODO: implement recursively.
     // - If open < 0 => false
+    if (open<0) return false;
     // - If idx == s.size() => open == 0
+    if (idx==s.size()) return open==0;
     // - If '(' => recurse open+1
+    if (s[idx]=='(') return is_balanced_rec(s, idx+1, open+1);
     // - If ')' => recurse open-1
+    if (s[idx]==')') return is_balanced_rec(s, idx+1, open-1);
     // - else ignore
+    else return is_balanced_rec(s, idx+1, open);
     return false;
 }
 
